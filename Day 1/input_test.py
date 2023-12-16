ed9de7c125e886ff75712475a52d9c7054e45b44
DoubleSC <153965067+DoubleSC@users.noreply.github.com>
2023-12-16 13:46:47 +0900

    Add files via upload

---
+++ Day 1/input_test.py
@@ -0,0 +1,13 @@
+coffee = input("내가 좋아하는 커피:")
+print(f"당신이 좋아하는 커피는 {coffee}군요")
+
+
+
+# = 왼쪽에 있는 데이터를 오른쪽에 넣는다.
+# 변수
+# abc = 1
+# print(abc)
+# print(f"abc {abc}")
+
+name = input("당신의 이름은 무엇입니까?")
+print(f"""{name} 작가는 영화 시장이 어려울수록 제일 만들기 어려""")
---
+++ Day 1/type_test.py
@@ -0,0 +1,13 @@
+a = int(input("첫번째 숫자는?"))
+b = int(input("두번째 숫자는?"))
+c = int(input("세번째 숫자는?"))
+
+print(f"세 숫자의 곱은 {a*b*c}입니다")
+
+
+
+# int - 정수
+
+
+
+
